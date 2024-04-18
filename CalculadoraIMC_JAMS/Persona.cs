a7b6d839ff8545ff506c52630970387ca424511d
medinacam <medinampos353@gmail.com>
2024-04-18 15:57:00 -0700

    Definir la clase persona

---
+++ CalculadoraIMC_JAMS/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraIMC_JAMS
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+        public string Edad { get; set; }
+        public float Estatura { get; set; }
+        public float Peso { get; set; }
+
+    }
+}
